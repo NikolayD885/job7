180524c02f14faae93a951b2a007bc9e0a2d3e7b
NikolayD <kolik885@ya.ru>
2022-09-01 22:23:19 +0700

    Программа к заданию №47, изменение

--- 047/Program.cs
+++ 047/Program.cs
@@ -5,39 +5,47 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-void PrintArray(double[,] array) { 
-    for (var i = 0; i < array.GetLength(0); i++) { 
-        for (var j = 0; j < array.GetLength(1); j++) { 
-            Console.Write($"{array[i,j]. ToString("N1")}, "); 
-        } 
-        Console.WriteLine(); 
-    } 
-} 
- 
-double[,] CreateArrayWithRandomNumbers(int m, int n) { 
-    double[,] result = new double[m,n]; 
- 
-    var random = new Random(); 
- 
-    for (var i = 0; i < result.GetLength(0); i++) { 
-        for (var j = 0; j < result.GetLength(1); j++) { 
-            result[i,j] = random.NextDouble()*10; 
-        } 
-    } 
- 
-    return result; 
-} 
+void PrintArray(double[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j].ToString("N1")}, ");
+        }
+        Console.WriteLine();
+    }
+}
 
-Console.Clear(); 
-Console.WriteLine("Введите число строк (m)"); 
-if(!int.TryParse(Console.ReadLine()!, out var m)) { 
-    Console.WriteLine("Всё плохо"); 
-} 
- 
-Console.WriteLine("Введите число столбцов (n)"); 
-if(!int.TryParse(Console.ReadLine()!, out var n)) { 
-    Console.WriteLine("Всё плохо"); 
-} 
- 
- 
+double[,] CreateArrayWithRandomNumbers(int m, int n)
+{
+    double[,] result = new double[m, n];
+
+    var random = new Random();
+
+    for (var i = 0; i < result.GetLength(0); i++)
+    {
+        for (var j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = random.NextDouble() * 10;
+        }
+    }
+
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число строк (m)");
+if (!int.TryParse(Console.ReadLine()!, out var m))
+{
+    Console.WriteLine("Всё плохо");
+}
+
+Console.WriteLine("Введите число столбцов (n)");
+if (!int.TryParse(Console.ReadLine()!, out var n))
+{
+    Console.WriteLine("Всё плохо");
+}
+
+
 PrintArray(CreateArrayWithRandomNumbers(m, n));
